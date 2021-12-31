74dca87e962c7904b26edf86c17fd4ca32a1111b
Andres-Dead <2118100663@soy.utj.edu.mx>
2021-12-30 23:24:00 -0600

    Starting the proyect 🐏🐏

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [{ path: 'administratives', loadChildren: () => import('./administratives/administratives.module').then(m => m.AdministrativesModule) }, { path: 'couriers', loadChildren: () => import('./couriers/couriers.module').then(m => m.CouriersModule) }, { path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) }, { path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule) }];
+const routes: Routes = [
+  {
+    path: "administratives",
+    loadChildren: () =>
+      import("./modules/administratives/administratives.module").then(
+        (m) => m.AdministrativesModule
+      ),
+  },
+  {
+    path: "couriers",
+    loadChildren: () =>
+      import("./modules/couriers/couriers.module").then((m) => m.CouriersModule),
+  },
+  {
+    path: "users",
+    loadChildren: () =>
+      import("./modules/users/users.module").then((m) => m.UsersModule),
+  },
+  {
+    path: "login",
+    loadChildren: () =>
+      import("./modules/login/login.module").then((m) => m.LoginModule),
+  },
+  { path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
